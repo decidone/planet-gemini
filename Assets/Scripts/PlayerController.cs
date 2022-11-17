1996da8b26fcf0ecbe57f4119bf586380545f1a4
decidone <decidone@gmail.com>
2022-11-17 18:06:54 +0900

    플레이어 아이템 인식 범위 설정

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.name);
+    }
+}
