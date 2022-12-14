554d12e33c8cb85e40de066f85f5cab91eeb6f8e
decidone <decidone@gmail.com>
2022-12-14 18:41:23 +0900

    아이템 루팅 방식을 리스트로 변경

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,15 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
-    private bool lootEnabled = false;
+    public List<GameObject> items = new List<GameObject>();
 
     private void Update()
     {
-        if (Input.GetButtonDown("Loot"))
-            lootEnabled = true;
-        if (Input.GetButtonUp("Loot"))
-            lootEnabled = false;
+        if (Input.GetButton("Loot"))
+            Loot();
 
         // 인벤토리가 열려있을 때 마우스 클릭 처리
         // 일단 여기 넣었지만 화면 클릭을 통한 무언가를 다른 스크립트에서 처리한다면 그쪽으로 이동
@@ -18,17 +17,35 @@ public class PlayerController : MonoBehaviour
             return;
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (lootEnabled)
+        ItemProps itemProps = collision.GetComponent<ItemProps>();
+        if(itemProps)
+            items.Add(collision.gameObject);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        ItemProps itemProps = collision.GetComponent<ItemProps>();
+        if (itemProps && items.Contains(collision.gameObject))
+            items.Remove(collision.gameObject);
+    }
+
+    private void Loot()
+    {
+        foreach (GameObject item in items)
         {
-            ItemProps itemProps = collision.GetComponent<ItemProps>();
+            ItemProps itemProps = item.GetComponent<ItemProps>();
             if (itemProps)
             {
                 // 인벤토리에 넣음
                 bool wasPickedUp = Inventory.instance.Add(itemProps.item, itemProps.amount);
                 if (wasPickedUp)
-                    Destroy(collision.gameObject);
+                {
+                    items.Remove(item);
+                    Destroy(item);
+                    break;
+                }
             }
         }
     }
