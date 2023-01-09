1dd5fd09804a985e7312daab5a0264c0e70ac4ee
avort <avorton1024@gmail.com>
2023-01-09 18:56:02 +0900

    벨트 코드 수정 및 Raycast 2D 옵션값 변경
    벨트 매니저, 벨트 그룹, 벨트 별 스크립트로 변경

--- Assets/Algen/Scripts/BeltCtrl.cs
+++ Assets/Algen/Scripts/BeltCtrl.cs
@@ -1,144 +1,151 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+public enum BeltState
+{
+    SoloBelt,
+    StartBelt,
+    EndBelt,
+    RepeaterBelt,
+}
 public class BeltCtrl : MonoBehaviour
 {
-    public float dirNum = 0;    // 방향
-    public float modelNum = 0;  // 모션
+    public int dirNum = 0;    // 방향
+    public int modelNum = 0;  // 모션
+
+    public GameObject beltGroupMgr;
+    GameObject beltManager = null;
 
     protected Animator anim;
     protected Animator animsync;
 
-    public float[] otherBeltNum = new float[] { 4, 4, 4, 4 };  //0 : 상, 1 : 우, 2 : 하, 3 : 좌 , 4 없음
-    Vector2 pos;    // 현 위치
+    public BeltState beltState;
 
+    public bool isTurn = false;
+    public bool isRightTurn = true;
+    
+    public BeltCtrl beltInSequence;
     // Start is called before the first frame update
     private void Awake()
     {
-        animsync = GameObject.Find("BeltAnimSync").GetComponent<Animator>();
+        beltManager = GameObject.Find("BeltManager");
+        animsync = beltManager.GetComponent<Animator>();
         anim = GetComponent<Animator>();
+        beltState = BeltState.SoloBelt;
     }
-
     void Start()
     {
+        if (transform.parent.gameObject != null)
+            beltGroupMgr = transform.parent.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        pos = this.gameObject.transform.position;
-
         anim.SetFloat("DirNum", dirNum);
         anim.SetFloat("ModelNum", modelNum);
 
         anim.Play(0, -1, animsync.GetCurrentAnimatorStateInfo(0).normalizedTime);
+        ModelSet();
+
+        if (beltInSequence == null)
+            beltInSequence = NextBeltCheck();
+
+        else if(beltInSequence != null)
+            CheckGroup();
     }
 
-    void ModelSelFunc()
+    void ModelSet()
     {
-        if (dirNum == 0 || dirNum == 2) // 방향이 위 아래 일 때 
+        if (isTurn == false)
         {
-            if((otherBeltNum[1] == 3 && otherBeltNum[3] == 1) || (otherBeltNum[1] == 4 && otherBeltNum[3] == 4))
-            {   // 오른쪽, 왼쪽 둘다 벨트가 있거나 없을 때
-                if (otherBeltNum[0] != 4 && otherBeltNum[2] != 4)       // 위랑 아래에 밸트가 있을 때
-                    modelNum = 2;
-                else if (otherBeltNum[0] != 4 && otherBeltNum[2] == 4)  // 위에만 벨트가 있을 때
-                    modelNum = 3;
-                else if (otherBeltNum[0] == 4 && otherBeltNum[2] != 4)  // 아래만 벨트가 있을 때
-                    modelNum = 1;
-                else
-                    modelNum = 0;
+            if (beltState == BeltState.SoloBelt)
+            {
+                modelNum = 0;
             }
-            else if (otherBeltNum[1] == 3)  // 오른쪽(otherBeltNum[1])에 왼쪽으로 가는 벨트(3)가 있을 때
-                modelNum = 4;
-            else if (otherBeltNum[3] == 1)  // 왼쪽(otherBeltNum[3])에 오른쪽으로 가는 벨트(1)가 있을 때
-                modelNum = 5;
+            else if (beltState == BeltState.StartBelt)
+            {
+                modelNum = 1;
+            }
+            else if (beltState == BeltState.EndBelt)
+            {
+                modelNum = 3;
+            }
+            else if (beltState == BeltState.RepeaterBelt)
+            {
+                modelNum = 2;
+            }
         }
-        else if (dirNum == 1 || dirNum == 3)    // 방향이 오른쪽 왼쪽 일 때 
+        else if (isTurn == true)
         {
-            if ((otherBeltNum[0] == 2 && otherBeltNum[2] == 0) || (otherBeltNum[0] == 4 && otherBeltNum[2] == 4))
-            {   // 위, 아래 둘다 벨트가 있거나 없을 때
-                if (otherBeltNum[1] != 4 && otherBeltNum[3] != 4)       // 오른쪽랑 왼쪽에 밸트가 있을 때
-                    modelNum = 2;
-                else if (otherBeltNum[1] == 4 && otherBeltNum[3] != 4)  // 왼쪽에만 벨트가 있을 때
-                    modelNum = 3;
-                else if (otherBeltNum[1] != 4 && otherBeltNum[3] == 4)  // 오른쪽에만 벨트가 있을 때
-                    modelNum = 1;
-                else
-                    modelNum = 0;
-            }
-            else if (otherBeltNum[0] == 2)  // 위(otherBeltNum[0])에 아래로 가는 벨트(2)가 있을 때
-                modelNum = 4;
-            else if (otherBeltNum[2] == 0)  // 아래(otherBeltNum[2])에 위로 가는 벨트(0)가 있을 때
+            if(isRightTurn == true)
+            {
                 modelNum = 5;
+            }
+            else if (isRightTurn == false)
+            {
+                modelNum = 4;
+            }
         }
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    private BeltCtrl NextBeltCheck() 
     {
-        if (collision.CompareTag("Belt"))
+        var nextCheck = transform.up;
+
+        if (dirNum == 0)
         {
-            if (collision.transform.position.x == pos.x)//같은 x 좌표고
-            {
-                if (collision.transform.position.y > pos.y)//y 좌표가 높으면
-                {
-                    otherBeltNum[0] = collision.GetComponentInChildren<BeltCtrl>().dirNum;
-                }
-                else if (collision.transform.position.y < pos.y)//y 좌표가 낮으면
-                {
-                    otherBeltNum[2] = collision.GetComponentInChildren<BeltCtrl>().dirNum;
-                }
-                else
-                    return;
-            }
-            else if (collision.transform.position.y == pos.y)//같은 y 좌표고
-            {
-                if (collision.transform.position.x > pos.x)//x 좌표가 높으면
-                {
-                    otherBeltNum[1] = collision.GetComponentInChildren<BeltCtrl>().dirNum;
-                }
-                else if (collision.transform.position.x < pos.x)//x 좌표가 낮으면
-                {
-                    otherBeltNum[3] = collision.GetComponentInChildren<BeltCtrl>().dirNum;
-                }
-                else
-                    return;
-            }
-            ModelSelFunc();
+            nextCheck = transform.up;
         }
+        else if (dirNum == 1)
+        {
+            nextCheck = transform.right;
+        }
+        else if (dirNum == 2)
+        {
+            nextCheck = -transform.up;
+        }
+        else if (dirNum == 3)
+        {
+            nextCheck = -transform.right;
+        }
+
+        RaycastHit2D hit = Physics2D.Raycast(this.gameObject.transform.position, nextCheck, 1f);
+
+        if (hit)
+        {
+            Debug.Log(hit.collider.gameObject.name);
+            BeltCtrl belt = hit.collider.GetComponent<BeltCtrl>();
+
+            if (belt != null)
+            {
+                return belt;
+            }
+        }
+
+        return null;
     }
-    void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Belt"))
+
+    void CheckGroup()
+    {// && dirNum == beltInSequence.dirNum
+        if(beltInSequence.beltGroupMgr != null && beltGroupMgr != beltInSequence.beltGroupMgr)
         {
-            if (collision.transform.position.x == pos.x)//같은 x 좌표고
+            if(dirNum % 2 == 0)
             {
-                if (collision.transform.position.y > pos.y)//y 좌표가 높으면
-                {
-                    otherBeltNum[0] = 4;
-                }
-                else if (collision.transform.position.y < pos.y)//y 좌표가 낮으면
-                {
-                    otherBeltNum[2] = 4;
-                }
+                if (beltInSequence.dirNum % 2 == 1)
+                    beltManager.GetComponent<BeltManager>().BeltCombine(beltGroupMgr, beltInSequence.beltGroupMgr);
                 else
                     return;
             }
-            else if (collision.transform.position.y == pos.y)//같은 y 좌표고
+            else if (dirNum % 2 == 1)
             {
-                if (collision.transform.position.x > pos.x)//x 좌표가 높으면
-                {
-                    otherBeltNum[1] = 4;
-                }
-                else if (collision.transform.position.x < pos.x)//x 좌표가 낮으면
-                {
-                    otherBeltNum[3] = 4;
-                }
+                if (beltInSequence.dirNum % 2 == 0)
+                    beltManager.GetComponent<BeltManager>().BeltCombine(beltGroupMgr, beltInSequence.beltGroupMgr);
                 else
                     return;
             }
-            ModelSelFunc();
+
         }
+
     }
 }
---
+++ Assets/Algen/Scripts/BeltManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class BeltManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BeltCombine(GameObject fstBelt, GameObject secBelt)
+    {
+        BeltGroupMgr fstGroupMgr = fstBelt.GetComponent<BeltGroupMgr>();
+        BeltGroupMgr secGroupMgr = secBelt.GetComponent<BeltGroupMgr>();
+
+        List<GameObject> result = new List<GameObject>();
+
+        //ÇÕÄ¡±â
+        result.AddRange(fstGroupMgr.BeltList);
+        result.AddRange(secGroupMgr.BeltList);
+
+        fstGroupMgr.BeltList.Clear();
+        fstGroupMgr.BeltList = result.Distinct().ToList();
+
+        foreach (GameObject belt in secGroupMgr.BeltList)
+        {
+            belt.transform.parent = fstGroupMgr.transform;
+            belt.GetComponent<BeltCtrl>().beltGroupMgr = fstBelt;
+        }
+
+        fstGroupMgr.Reconfirm();
+        Destroy(secBelt);
+    }
+}
