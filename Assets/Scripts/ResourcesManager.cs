064086ebafea7cbd17d272e2077d6981227fb64d
decidone <decidone@gmail.com>
2023-11-06 11:05:14 +0900

    fog of war + a
    1. 기존 mask vision을 맵ui에서만 보이도록 변경
    2. 경계면이 부드러운 시야 시스템
    3. 리소스 관리 스크립트 추가

---
+++ Assets/Scripts/ResourcesManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourcesManager : MonoBehaviour
+{
+    #region Singleton
+    public static ResourcesManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("More than one instance of ResourcesManager found!");
+            return;
+        }
+
+        instance = this;
+    }
+    #endregion
+}
