d37bb7a57721206effa5104e61b9c6f9e793fabd
decidone <decidone@gmail.com>
2025-02-18 18:59:09 +0900

    사전 ui 추가

---
+++ Assets/Scripts/UI/InfoDictionary.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoDictionary : MonoBehaviour
+{
+
+
+
+    public void OpenUI()
+    {
+
+    }
+
+    public void CloseUI()
+    {
+
+    }
+}
